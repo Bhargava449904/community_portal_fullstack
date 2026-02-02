5ff8fb5792300cc113c3b0b9b09e5f6077172bb8
dasari bhargava <bhargava449904>
2026-02-02 22:35:19 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,6 @@
-
-// import './App.css'
-// import {Home} from './Home'
-// function App() {
-
-
-//   return (
-//     <>
-//       <div>
-//         <Home/>
-//       </div>
-
-//     </>
-//   )
-// }
-
-// export default App
-
-
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
-import { Home } from "./Home";
-import AdminDashboard from "./admin/AdminDashboard";
-import CitizenDashboard from "./citizen/CitizenDashboard";
 import { Dashboard } from "./components/dashborad/Dashboard";
 
 function App() {
