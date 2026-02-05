77d4222e44e7f659c4c876b47f0acd95a58fdae4
dasari bhargava <bhargava449904>
2026-02-05 16:26:00 +0530

    citizendashboard

--- src/components/dashborad/Citizendash.jsx
+++ src/components/dashborad/Citizendash.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../../../src/components/dashborad/citizendash.css";
+import "./citizendash.css";
 
 function CitizenDash() {
   const [title, setTitle] = useState("");
